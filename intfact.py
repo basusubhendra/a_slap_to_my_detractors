34f003534f36e9c332edfa94da0133405f789bc5
Subhendra Basu <basu.subhendra@outlook.com>
2022-09-29 08:51:39 +0000

    Re-constructs factors in a piecewise linear fashion

--- intfact.py
+++ intfact.py
@@ -22,15 +22,14 @@ def characterize(num):
     tuples = []
     ctr = 0
     l = len(num)
-    while ctr < l - 1:
-       tuples.append(num[ctr] + num[ctr + 1])
+    while (ctr == 0) or ((ctr + 1) % l != 0): 
+       tuples.append(num[ctr % l] + num[(ctr + 1) % l])
        ctr = ctr + 1
     return tuples
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
     tuples = characterize(num)
-    print(tuples)
     pp = open("./pi.txt", "r")
     ee = open("./e.txt", "r")
     e_str = str(ee.read(2))
@@ -42,34 +41,36 @@ if __name__ == "__main__":
     factor1 = ""
     factor2 = ""
     accumulator = 0
+    ll = len(tuples)
     for x in range(0, 2):
        while True:
-           input([p_str, e_str])
            if _pivot_relation_ == _relation_(e_str[0], e_str[1]):
                accumulator = accumulator + 1
                _current_relation_ = _relation_(p_str[0], p_str[1])
                if _current_relation_ == _tuple_relation_:
-                   print(accumulator)
-                   sys.exit(1)
+                   input([tuples[ctr % ll],accumulator])
                    bin_str += bin(accumulator)[2:]
                    ctr = ctr + 1
+                   _tuple_relation_ = _relation_(tuples[ctr % ll][0], tuples[ctr % ll][1])
+                   accumulator = 0
+                   #Roles of pi and e are reversed
+                   tt = pp
+                   pp = ee
+                   ee = tt
                    e_str = str(ee.read(2))
                    p_str = str(pp.read(2))
+                   _pivot_relation_ = _relation_(e_str[0], e_str[1])
                    if ctr == len(tuples):
                         factor1 = bin_str
+                        bin_str = ""
                         break
-                   e_str = str(ee.read(2))
-                   p_str = str(pp.read(2))
-                   _tuple_relation_ = _relation_(tuples[ctr][0], tuples[ctr][1])
-                   _pivot_relation_ = _relation_(e_str[0], e_str[1])
-                   accumulator = 0
+                   elif ctr == 2*len(tuples):
+                        factor2 = bin_str
+                        break
            e_str = str(ee.read(2))
            p_str = str(pp.read(2))
-       tt = pp
-       pp = ee
-       ee = tt
-       _pivot_relation_ = _relation_(e_str[0], e_str[1])
-       ctr = 0
-    factor2 = bin_str
-    print(factor1)
-    print(factor2)
+    #TBD Factors to be printed
+    #print(factor1)
+    #print(factor2)
+    pp.close()
+    ee.close()
