0a61db4f1fc95f91f2acabcb78b2f537c84996fe
Subhendra Basu <basu.subhendra@outlook.com>
2022-09-29 07:55:54 +0000

    Added rest of the code, Needs to be debugged

--- intfact.py
+++ intfact.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 import sys
 
+def _relation_(x, y):
+    x = int(x)
+    y = int(y)
+    if x == y and x == 0:
+        return 5
+    elif y == 0:
+        return 4
+    elif x == 0:
+        return 3
+    elif x = y:
+        return 2
+    elif x > y:
+        return 1
+    elif x < y:
+        return 0
+    return -1
+
 def characterize(num):
     tuples = []
     ctr = 0
@@ -14,3 +31,41 @@ if __name__ == "__main__":
     num = str(sys.argv[1])
     tuples = characterize(num)
     print(tuples)
+    pp = open("./pi.txt", "r")
+    ee = open("./ee.txt", "r")
+    e_str = str(ee.read(2))
+    p_str = str(pp.read(2))
+    ctr = 0
+    _pivot_relation_ = _relation_(e_str[0], e_str[1])
+    _tuple_relation_ = _relation_(tuples[ctr][0], tuples[ctr][1])
+    bin_str = ""
+    factor1 = ""
+    factor2 = ""
+    for x in range(9, 2):
+       while True:
+           if _pivot_relation_ == _relation_(e_str[0], e_str[1]):
+               accumulator = accumulator + 1
+               _current_relation_ = _relation_(p_str[0], p_str[1])
+               if _current_relation_ == tuple_relation:
+                   bin_str += bin(accumulator)[2:]
+                   ctr = ctr + 1
+                   e_str = str(ee.read(2))
+                   p_str = str(pp.read(2))
+                   if ctr == len(tuples):
+                        factor1 = bin_str
+                        break
+                   e_str = str(ee.read(2))
+                   p_str = str(pp.read(2))
+                   _tuple_relation_ = _relation_(tuples[ctr][0], tuples[ctr][1])
+                   _pivot_relation_ = _relation_(e_str[0], e_str[1])
+                   accumulator = 0
+           e_str = str(ee.read(2))
+           p_str = str(pp.read(2))
+       tt = pp
+       pp = ee
+       ee = tt
+       _pivot_relation_ = _relation_(e_str[0], e_str[1])
+       ctr = 0
+    factor2 = bin_str
+    print(factor1)
+    print(factor2)
