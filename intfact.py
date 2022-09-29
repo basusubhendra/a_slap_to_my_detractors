86958cbadad6fa2613b3eb63ff188e611dc8bb53
Subhendra Basu <basu.subhendra@outlook.com>
2022-09-29 07:15:52 +0000

    Characterization code added

--- intfact.py
+++ intfact.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 import sys
 
+def characterize(num):
+    tuples = []
+    ctr = 0
+    l = len(num)
+    while ctr < l - 1:
+       tuples.append(num[ctr] + num[ctr + 1])
+       ctr = ctr + 1
+    return tuples
+
 if __name__ == "__main__":
     num = str(sys.argv[1])
+    tuples = characterize(num)
+    print(tuples)
